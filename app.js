c35699b9e47706bcae6ca17b0fbb0f6936a36fbd
zaater <almogtbanmr123@gmail.com>
2026-02-18 01:18:59 +0300

    fix: mongo and env in compose and app.ja

--- app.js
+++ app.js
@@ -18,9 +18,12 @@ app.use("/user", userRout);
 
 const start = async () => {
   try {
-    await connectDB(process.env.MONGO_URL);
+	const mongouri = `mongodb://${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/${process.env.MONGO_INITDB_DATABASE}`;
+	console.log(mongouri)
+    await connectDB(mongouri);
     console.log("connected to db");
-    app.listen(4000, console.log("server is running on port 3000"));
+	const APP_PORT = process.env.APP_PORT;
+    app.listen(APP_PORT, console.log("server is running on port " + APP_PORT));
   } catch (e) {
     console.log(e.message);
   }
